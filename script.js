a14e247c4cbb44b13f3a24525bd7966639205612
Phoebe Yap <Phoebe1305@outlook.com>
2024-07-03 13:18:15 +0800

    Added loading sign
    Content generation takes a while so added loading sign for users to know that it is loading and not an error.

---
+++ script.js
@@ -0,0 +1,7 @@
+let loading = document.querySelector('#loading')
+document.querySelector('#generate').addEventListener('click', function() {
+    loading.style.visibility='visible'
+});
+console.log('check')
+let body = document.querySelector('body')
+body.style.backgroundColor = 'red';
